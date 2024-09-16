da9fed3787526db672cf9e5a42ec91f52b27779b
Aleksey Profatilov <snakeduse@gmail.com>
2024-09-16 23:59:22 +0300

    feat: Настроил репликацию

--- mongo-sharding-repl/scripts/init_shard1.sh
+++ mongo-sharding-repl/scripts/init_shard1.sh
@@ -6,11 +6,14 @@
 
 docker compose exec -T mongo_shard1 mongosh --port 27022 <<EOF
 rs.initiate(
-    {
-      _id : "shard1",
-      members: [
-        { _id : 0, host : "mongo_shard1:27022" }
-      ]
-    }
+  {
+    _id : "shard1",
+    members: [
+      { _id : 0, host : "mongo_shard1:27022" },
+      { _id : 1, host : "mongo_shard1_r1:27024" },
+      { _id : 2, host : "mongo_shard1_r2:27025" },
+      { _id : 3, host : "mongo_shard1_r3:27026" }
+    ]
+  }
 )
 EOF
--- mongo-sharding-repl/scripts/init_shard2.sh
+++ mongo-sharding-repl/scripts/init_shard2.sh
@@ -6,11 +6,14 @@
 
 docker compose exec -T mongo_shard2 mongosh --port 27023 <<EOF
 rs.initiate(
-    {
-      _id : "shard2",
-      members: [
-        { _id : 0, host : "mongo_shard2:27023" }
-      ]
-    }
+  {
+    _id : "shard2",
+    members: [
+      { _id : 0, host : "mongo_shard2:27023" },
+      { _id : 1, host : "mongo_shard2_r1:27027" },
+      { _id : 2, host : "mongo_shard2_r2:27028" },
+      { _id : 3, host : "mongo_shard2_r3:27029" }
+    ]
+  }
 )
 EOF
